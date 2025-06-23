b5d06e28f3586b536fa51616cac6d57b27e7fe5e
Edanur Korkmaz <edanuurkorkmaz@gmail.com>
2025-06-23 14:20:05 +0300

    feat: started creating portfolio using Next.js

---
+++ src/app/layout.jsx
@@ -0,0 +1,30 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+    variable: "--font-geist-sans",
+    subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+    variable: "--font-geist-mono",
+    subsets: ["latin"],
+});
+
+export const metadata = {
+    title: "Edanur Korkmaz | Frontend Developer",
+    description:
+        "Explore the personal portfolio of Edanur Korkmaz, a frontend developer specializing in modern web technologies like React, Next.js, and Tailwind CSS.",
+};
+
+export default function RootLayout({ children }) {
+    return (
+        <html lang="en">
+            <body
+                className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+            >
+                {children}
+            </body>
+        </html>
+    );
+}
